09b5eb3216bf13b66e6d85ba9ea003e1c2633038
kaweeracha <kaweeracha@MacBook-Air-khxng-kaweeracha.local>
2023-12-01 00:49:00 +0700

    finish:Exercise123456

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,13 @@
 let numbers = [100, 20, 3, 1000];
 let minNumber;
 // Start coding here
-
-console.log(minNumber);
+if (numbers.length > 0) {
+    minNumber = numbers[0]; 
+  
+    for (let i = 1; i < numbers.length; i++) {
+      if (numbers[i] < minNumber) {
+        minNumber = numbers[i]; 
+      }
+    }
+  }
+  console.log(minNumber);
--- ex-4.js
+++ ex-4.js
@@ -1,6 +1,10 @@
 // Exercise #4: Displaying Restaurant Name
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
+let i = 0;
 // Start coding here
-
+while (i < restaurants.length) {
+    newRestaurants.push(`Restaurant Name: ${restaurants[i]}`);
+    i++;
+  }
 console.log(newRestaurants);
